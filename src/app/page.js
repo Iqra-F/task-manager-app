85ddce35bc75c8b811a6dd0dc9483d6c300bb70f
Fatima_Iqra <iqraf683@gmail.com>
2025-09-13 13:43:59 +0500

    Initial setup and folder structure

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-2xl text-pink-500 flex items-center justify-center w-screen h-screen">
+      Salam!
+      it's a task management app. welcome!
+    </div>
+  );
+}
